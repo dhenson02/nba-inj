d025509038e7755a23f929465659716b519a7da4
Deryck H <dhenson@viztek.net>
2026-01-14 16:24:22 -0500

    fix downloader - i guess it needs user agent now?

--- report-utils/downloader.js
+++ report-utils/downloader.js
@@ -42,6 +42,15 @@ export async function downloadFile ( date, time ) {
             method: 'get',
             url,
             responseType: 'stream',
+            headers: {
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Cache-Control": "max-age=0",
+                "Connection": "keep-alive",
+                "Host": "ak-static.cms.nba.com",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+            },
         });
         const fileStream = fs.createWriteStream(localFilePath);
         response.data.pipe(fileStream);
